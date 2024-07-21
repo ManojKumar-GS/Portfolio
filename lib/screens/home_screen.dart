e1c97ae93b4756ba28598230766028950e25f8c1
ManojKumar-GS <manojkumargs195@gmail.com>
2024-07-21 12:18:24 +0530

    icons impl

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:portfolio/screens/more_info.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -20,54 +23,95 @@ class _MyHomePageState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          drawer: Drawer(
-            backgroundColor: Colors.transparent,
-            width: 200,
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: [
-                const SizedBox(
-                  height: 80,
+          drawer: BackdropFilter(
+            filter: ImageFilter.blur(sigmaY: 5, sigmaX: 5),
+            child: Drawer(
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              width: 200,
+              child: SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.2,
+                child: ListView(
+                  shrinkWrap: true,
+                  padding: EdgeInsets.zero,
+                  children: [
+                    const SizedBox(
+                      height: 80,
+                    ),
+                    ListTile(
+                      minVerticalPadding:
+                          MediaQuery.sizeOf(context).height * 0.05,
+                      textColor: Colors.black,
+                      leading: Image.asset("assets/icons/education.png"),
+                      title: const Text(
+                        'Education',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white,
+                            fontFamily: AutofillHints.birthday),
+                      ),
+                      onTap: () {
+                        _onItemTapped(0);
+                        Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      minVerticalPadding:
+                          MediaQuery.sizeOf(context).height * 0.05,
+                      leading: Image.asset("assets/icons/exp.png"),
+                      title: const Text(
+                        'Experience',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white,
+                            fontFamily: AutofillHints.birthday),
+                      ),
+                      onTap: () {
+                        _onItemTapped(1);
+                        Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      minVerticalPadding:
+                          MediaQuery.sizeOf(context).height * 0.05,
+                      leading: Image.asset("assets/icons/skills.png"),
+                      title: const Text(
+                        'Skills',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white,
+                            fontFamily: AutofillHints.birthday),
+                      ),
+                      onTap: () {
+                        _onItemTapped(2);
+                        Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      minVerticalPadding:
+                          MediaQuery.sizeOf(context).height * 0.05,
+                      leading: Image.asset("assets/icons/hobby.png"),
+                      title: const Text(
+                        'Hobbies',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white,
+                            fontFamily: AutofillHints.birthday),
+                      ),
+                      onTap: () {
+                        _onItemTapped(2);
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
                 ),
-                ListTile(
-                  textColor: Colors.black,
-                  leading:
-                      const Icon(Icons.school, size: 50, color: Colors.black),
-                  title: const Text('Education'),
-                  selected: _selectedIndex == 0,
-                  onTap: () {
-                    _onItemTapped(0);
-                    Navigator.pop(context);
-                  },
-                ),
-                ListTile(
-                  minVerticalPadding: MediaQuery.sizeOf(context).height * 0.1,
-                  leading:
-                      const Icon(Icons.school, size: 50, color: Colors.black),
-                  title: const Text('Experience'),
-                  selected: _selectedIndex == 1,
-                  onTap: () {
-                    _onItemTapped(1);
-                    Navigator.pop(context);
-                  },
-                ),
-                ListTile(
-                  leading:
-                      const Icon(Icons.school, size: 50, color: Colors.black),
-                  title: const Text('Hobbies'),
-                  selected: _selectedIndex == 2,
-                  onTap: () {
-                    _onItemTapped(2);
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
+              ),
             ),
           ),
           body: CustomScrollView(
             slivers: [
               SliverAppBar(
-                collapsedHeight: MediaQuery.sizeOf(context).height * 0.17,
+                collapsedHeight: MediaQuery.sizeOf(context).height * 0.18,
                 surfaceTintColor: Colors.black,
                 backgroundColor: Theme.of(context).colorScheme.inversePrimary,
                 shape: const RoundedRectangleBorder(
@@ -77,32 +121,42 @@ class _MyHomePageState extends State<HomeScreen> {
                 flexibleSpace: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Card(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(
-                              MediaQuery.sizeOf(context).height * 0.12)),
-                      child: SizedBox(
-                          width: MediaQuery.sizeOf(context).width * 0.4,
-                          height: MediaQuery.sizeOf(context).height * 0.18,
-                          child: Image.asset("assets/profile.png",
-                              fit: BoxFit.fitHeight)),
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => const MoreInfo()));
+                      },
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                MediaQuery.sizeOf(context).height * 0.12)),
+                        child: SizedBox(
+                            width: MediaQuery.sizeOf(context).width * 0.4,
+                            height: MediaQuery.sizeOf(context).height * 0.18,
+                            child: Image.asset("assets/profile.png",
+                                fit: BoxFit.fitHeight)),
+                      ),
                     ),
-                    const Expanded(
+                    Expanded(
                       child: Padding(
                         padding: EdgeInsets.all(15.0),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Padding(
+                            const Padding(
                               padding: EdgeInsets.all(8.0),
                               child: Text("Manoj Kumar GS",
                                   style: TextStyle(fontSize: 25)),
                             ),
                             Row(
                               children: [
-                                Icon(Icons.phone),
-                                Padding(
+                                Image.asset(
+                                  "assets/icons/phone.png",
+                                  height: 30,
+                                  width: 30,
+                                ),
+                                const Padding(
                                   padding: EdgeInsets.only(left: 8.0),
                                   child: Text(
                                     "+91 9481878819",
@@ -117,9 +171,30 @@ class _MyHomePageState extends State<HomeScreen> {
                                 mainAxisAlignment:
                                     MainAxisAlignment.spaceEvenly,
                                 children: [
-                                  Icon(Icons.message),
-                                  Icon(Icons.link),
-                                  Icon(Icons.mail),
+                                  InkWell(
+                                    child: Image.asset(
+                                      "assets/icons/gmail.png",
+                                      height: 30,
+                                      width: 30,
+                                    ),
+                                    onTap: () {},
+                                  ),
+                                  InkWell(
+                                    child: Image.asset(
+                                      "assets/icons/linkdin.png",
+                                      height: 30,
+                                      width: 30,
+                                    ),
+                                    onTap: () {},
+                                  ),
+                                  InkWell(
+                                    child: Image.asset(
+                                      "assets/icons/github.png",
+                                      height: 30,
+                                      width: 30,
+                                    ),
+                                    onTap: () {},
+                                  ),
                                 ],
                               ),
                             ),
