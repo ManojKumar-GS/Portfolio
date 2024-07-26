7894136250796026868cd3d82fce5eaf5f7b5642
ManojKumar-GS <manojkumargs195@gmail.com>
2024-07-26 22:55:45 +0530

    theme mode added

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
