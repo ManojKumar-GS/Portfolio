555263fd992c7a45c5bd00edd9c9ea4c9e3468ce
ManojKumar-GS <manojkumargs195@gmail.com>
2024-08-23 09:00:45 +0530

    content added

--- lib/screens/about_me.dart
+++ lib/screens/about_me.dart
@@ -23,16 +23,42 @@ class _AboutMeState extends State<AboutMe> {
               end: Alignment.bottomCenter,
               colors: [Colors.white, Colors.black],
             )),
-            child: const Column(
+            child: Column(
               children: [
-                Text(
+                const Text(
                   "I'm Manoj Kumar GS",
-                  style: TextStyle(color: Colors.black),
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900,
+                      height: 4),
                 ),
-                Text(
+                const Text(
                   "Full Stack Developer",
-                  style: TextStyle(color: Colors.black),
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w900,
+                      fontSize: 20),
                 ),
+                const SizedBox(height: 5),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.sizeOf(context).width * 0.1),
+                  child: const Text(
+                    "It is a ent of a page when looking at its layout. \n The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.\n Many desktop publishing packages , and a search for 'lorem ipsum' will uncover many web sites still in their infancy.\n Various versions have evolved sometimes on purpose (injected humour and the like)..",
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                  child: const Text("Resume",
+                      style: TextStyle(color: Colors.white)),
+                )
               ],
             ),
           ),
