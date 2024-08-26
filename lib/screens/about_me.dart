7edc4aa7927e79e4f36f85410070b386213e36fe
ManojKumar-GS <manojkumargs195@gmail.com>
2024-08-26 23:00:36 +0530

    about me added

--- lib/screens/about_me.dart
+++ lib/screens/about_me.dart
@@ -12,70 +12,120 @@ class _AboutMeState extends State<AboutMe> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: MediaQuery.sizeOf(context).height * 0.43,
-            width: MediaQuery.sizeOf(context).width,
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Colors.white, Colors.black],
-            )),
-            child: Column(
-              children: [
-                const Text(
-                  "I'm Manoj Kumar GS",
-                  style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w900,
-                      height: 4),
-                ),
-                const Text(
-                  "Full Stack Developer",
-                  style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.w900,
-                      fontSize: 20),
-                ),
-                const SizedBox(height: 5),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: MediaQuery.sizeOf(context).width * 0.1),
-                  child: const Text(
-                    "It is a ent of a page when looking at its layout. \n The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.\n Many desktop publishing packages , and a search for 'lorem ipsum' will uncover many web sites still in their infancy.\n Various versions have evolved sometimes on purpose (injected humour and the like)..",
-                    textAlign: TextAlign.justify,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: const Text(
+          "I'm Manoj Kumar GS",
+          style: TextStyle(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w900,
+              height: 4),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.sizeOf(context).height * 0.43,
+              width: MediaQuery.sizeOf(context).width,
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.white, Colors.black],
+              )),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text(
+                    "Full Stack Developer",
                     style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w400),
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w900,
+                        fontSize: 20),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-                  child: const Text("Resume",
-                      style: TextStyle(color: Colors.white)),
-                )
-              ],
+                  const SizedBox(height: 5),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: MediaQuery.sizeOf(context).width * 0.1),
+                    child: const Text(
+                      "It is a ent of a page when looking at its layout. \n The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.\n Many desktop publishing packages , and a search for 'lorem ipsum' will uncover many web sites still in their infancy.\n Various versions have evolved sometimes on purpose (injected humour and the like)..",
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                    child: const Text("Resume",
+                        style: TextStyle(color: Colors.white)),
+                  )
+                ],
+              ),
             ),
-          ),
-          Container(
-            height: MediaQuery.sizeOf(context).height * 0.1,
-            color: Colors.black,
-          ),
-          Container(
-            height: MediaQuery.sizeOf(context).height * 0.44,
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Colors.black, Colors.white],
-            )),
-          ),
-        ],
+            Container(
+              height: MediaQuery.sizeOf(context).height * 0.1,
+              width: MediaQuery.sizeOf(context).width,
+              color: Colors.black,
+              child: const Center(
+                child: Text(
+                  "About Me",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
+                ),
+              ),
+            ),
+            Container(
+              height: MediaQuery.sizeOf(context).height,
+              width: MediaQuery.sizeOf(context).width,
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.black, Colors.white],
+              )),
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.sizeOf(context).width * 0.42,
+                    height: MediaQuery.sizeOf(context).height * 0.2,
+                    child: Card(
+                      elevation: 15,
+                      shadowColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              MediaQuery.sizeOf(context).height * 0.12)),
+                      child: Image.asset("assets/images/profile.png",
+                          fit: BoxFit.fitHeight),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: MediaQuery.sizeOf(context).width * 0.1),
+                    child: const Text(
+                      "Dedicated and results-driven Software Developer with 1.5+years of hands-on experience in building Scalable and efficient applications. Specialized in Flutter for cross-platform mobile development, Node.js for backend services, gRPC for efficient communication, and Neo4j for graph database management.Possessing a strong foundation in Java, SQL, HTML, CSS, and JavaScript.",
+                      // textAlign: TextAlign.justify,
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     ));
   }
